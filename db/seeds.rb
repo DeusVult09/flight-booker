d98f57cc2d6c25009c81f608e9851e5b73686263
Zarifa <mamadalievazara@gmail.com>
2026-06-05 00:27:40 +0200

    Set up sample data in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Airport.create!(code: 'TAS')
+Airport.create!(code: 'RIX')
+Airport.create!(code: 'WAW')
+Airport.create!(code: 'JFK')
+Airport.create!(code: 'KRK')
