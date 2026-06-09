74bb8a7036d5237acdf4803b0b47b5d6f5c939c7
Zarifa <mamadalievazara@gmail.com>
2026-06-10 00:07:11 +0200

    Fix typo: 500 error encounter

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,10 +1,16 @@
 class FlightsController < ApplicationController
   def index
     @airports = Airport.all
+    @flight_dates = flight_date_formatted.map { |date| date.strftime("%d/%m/%Y") }
   end
 
   private 
-  def flights_params
-    params.expect(flight: [ :start_time, :departure_airport_id, :arrival_airport_id, ])
+
+  def flight_date_formatted
+    (Date.today..5.days.from_now).to_a
   end
+
+  #def flights_params
+    #params.expect(flight: [ :start_time, :departure_airport_id, :arrival_airport_id, ])
+  #end
 end
--- app/models/airport.rb
+++ app/models/airport.rb
@@ -2,5 +2,5 @@ class Airport < ApplicationRecord
   validates :code, presence: true, uniqueness: true, length: { maximum: 3 }
 
   has_many :departing_flights, class_name: "Flight", foreign_key: "departure_airport_id", dependent: :destroy
-  has_many :arriving_flights, class_name: "Flight", foreign_key: "arriving_airport_id", dependednt: :destroy
+  has_many :arriving_flights, class_name: "Flight", foreign_key: "arriving_airport_id", dependent: :destroy
 end
