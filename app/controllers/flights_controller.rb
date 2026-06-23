d9d7f4a0ad15597e106fdb4c2984d0f130361591
Zarifa <mamadalievazara@gmail.com>
2026-06-24 01:21:12 +0200

    Build searching flights query. Test -> successful

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,12 +3,12 @@ class FlightsController < ApplicationController
     @airports = Airport.all
     @flights = Flight.all
 
-    @flight_dates = @flights.map(&:start_time)
-    @flight_date_formatted = @flight_dates.map { |f| f.strftime(('%A, %B %e (%Y)'))}.uniq
-  end
-  
+   
 
-  # def flights_params
-  # params.expect(flight: [ :start_time, :departure_airport_id, :arrival_airport_id, ])
-  # end
+    @search_flights = Flight.where(
+      arrival_airport_id: params[:arrival_airport_id], 
+      departure_airport_id: params[:departure_airport_id])
+  end
+
+
 end
