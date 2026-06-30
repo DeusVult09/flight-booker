89fa58e180579ce77d3b8c18f60fac5bf558b330
Zarifa <mamadalievazara@gmail.com>
2026-07-01 01:16:07 +0200

    Convert raw integer duration to human readable format -> 00h/00m

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -5,4 +5,8 @@ class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
   has_many :bookings, dependent: :destroy
+
+  def human_duration
+    "#{duration / 60}h #{duration % 60}m"
+  end
 end
