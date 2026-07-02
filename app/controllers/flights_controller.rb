9a74abf213872d337ce1c5f0c41c4c65c98f9a10
Zarifa <mamadalievazara@gmail.com>
2026-07-03 01:36:33 +0200

    Fix: db date and search menu date mismatch. Parse the date object back to numbers and apply datetime in range

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,13 +2,13 @@ class FlightsController < ApplicationController
   def index
     @airports = Airport.all
     @flights = Flight.all
-
+    @flight_dates = @flights.map { |f| f.start_time.to_date }.uniq
    
 
     @search_flights = Flight.where(
       arrival_airport_id: params[:arrival_airport_id], 
-      departure_airport_id: params[:departure_airport_id]
-      )
+      departure_airport_id: params[:departure_airport_id])
+      .where(start_time: Date.parse(params[:start_time]).all_day)
   end
 
 
