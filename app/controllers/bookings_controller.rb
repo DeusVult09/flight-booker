0796af41753d9047cbe7771cb78528b261008473
Zarifa <mamadalievazara@gmail.com>
2026-07-06 01:15:14 +0200

    Build new and create actions for a booking to render form for passengers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,14 +2,22 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new
     @flight = Flight.find(params[:flight_id])
-    @passengers_num = params[:passengers_number]
+    @passengers_num = params[:passengers_number].to_i
   end
 
   def create
+    @passengers_num.times do
+      @booking.passengers.build
+    end
 
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
-
   end
+
 end
--- app/controllers/passenger_controller.rb
+++
@@ -1,4 +0,0 @@
-class PassengerController < ApplicationController
-  def new
-  end
-end
