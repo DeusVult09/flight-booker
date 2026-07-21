e81526651671d620a5f243699e90acaa1a0facc9
Zarifa <mamadalievazara@gmail.com>
2026-07-22 00:39:51 +0200

    Move empty search flight before if

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,15 +4,15 @@ class FlightsController < ApplicationController
     @flights = Flight.all
     @flight_dates = @flights.map { |f| f.start_time.to_date }.uniq
 
+    @flight_dates = Flight.none
+
     if params[:start_time].present?
       date = Date.parse(params[:start_time])
-
+      
       @search_flights = Flight.where(
       arrival_airport_id: params[:arrival_airport_id], 
       departure_airport_id: params[:departure_airport_id],
       start_time: date.all_day)
-    else
-      @flight_dates = Flight.none
     end    
   end
 end
