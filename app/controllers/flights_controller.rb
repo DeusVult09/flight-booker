1ae1beb8ac28c6bb0d7ae8209807b2e0bc4beac3
Zarifa <mamadalievazara@gmail.com>
2026-07-22 00:33:23 +0200

    Write a condition to excpect Active record trigger when flights dates are not selected

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,13 +3,16 @@ class FlightsController < ApplicationController
     @airports = Airport.all
     @flights = Flight.all
     @flight_dates = @flights.map { |f| f.start_time.to_date }.uniq
-   
 
-    @search_flights = Flight.where(
+    if params[:start_time].present?
+      date = Date.parse(params[:start_time])
+
+      @search_flights = Flight.where(
       arrival_airport_id: params[:arrival_airport_id], 
-      departure_airport_id: params[:departure_airport_id])
-      .where(start_time: Date.parse(params[:start_time]).all_day)
+      departure_airport_id: params[:departure_airport_id],
+      start_time: date.all_day)
+    else
+      @flight_dates = Flight.none
+    end    
   end
-
-
 end
