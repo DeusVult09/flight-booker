18480c0a9aeec50373bf1c3344c9edd4841dde12
Zarifa <mamadalievazara@gmail.com>
2026-07-21 19:01:06 +0200

    Change database from Sql Lite to Postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.1].define(version: 2026_07_03_231603) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
